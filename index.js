b8409f36fb570d392506a723b943e92ef842c639
Batuhan Balta <bthn.balta@gmail.com>
2020-03-18 20:47:54 -0700

    update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const server = require("./api/server.js");
+require("dotenv").config();
 
-const port = 5000;
+const port = process.env.PORT;
+
+server.get("/", (req, res) => {
+  console.log("this is homepage");
+  res.status(200).send("Selamlar");
+});
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
